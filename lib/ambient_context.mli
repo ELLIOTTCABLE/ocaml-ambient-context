2e67c61360c74523685043fae4d602e83835fe1a
Simon Cruanes <simon.cruanes.2007@m4x.org>
2023-09-19 14:49:51 -0400

    docs docs docs

--- lib/ambient_context.mli
+++ lib/ambient_context.mli
@@ -1,15 +1,35 @@
+(** Ambient context.
+
+    The ambient context, like the Matrix, is everywhere around you *)
+
 module type STORAGE = Types.STORAGE
 
 type storage = (module STORAGE)
 
 val default_storage : Types.storage
 val get_current_storage : unit -> Types.storage
+
 val with_storage_provider : Types.storage -> (unit -> unit) -> unit
+(** [with_storage_provider storage cb] sets [storage] as current storage, calls [cb()],
+    and restores current storage to its previous value. *)
 
 type 'a key
+(** A key that can be mapped to values of type ['a] in the ambient context. *)
 
 val compare_key : int -> int -> int
+(** Total order on keys *)
+
 val create_key : unit -> 'a key
+(** Create a new fresh key, distinct from any previously created key. *)
+
 val get : 'a key -> 'a option
+(** Get the current value for a given key, or [None] if no value was associated with the
+    key in the ambient context. *)
+
 val with_binding : 'a key -> 'a -> (unit -> 'r) -> 'r
+(** [without_binding k v cb] calls [cb()] in a context in which [k] is bound to [v]. When
+    [cb()] returns, storage is restored to its previous state. *)
+
 val without_binding : 'a key -> (unit -> 'b) -> 'b
+(** [without_binding k cb] calls [cb()] in a context where [k] has no binding (possibly
+    shadowing the current ambient binding of [k] if it exists). *)
--- lib/types.ml
+++ lib/types.ml
@@ -1,10 +1,43 @@
+(** Storage implementation.
+
+    The first-class module [(module STORAGE)] is the singleton responsible for {i storing}
+    data within the ambient context.
+
+    It is responsible for keeping track of that context in a manner that's consistent with
+    the program's choice of control flow paradigm:
+
+    - for synchronous/threaded/direct style code, {b TLS} ("thread local storage") keeps
+      track of a global variable per thread. Each thread has its own copy of the variable
+      and updates it independently of other threads.
+
+    - for Lwt, any ['a Lwt.t] created inside the [with_binding k v (fun _ -> …)] will
+      inherit the [k := v] assignment.
+
+    - for Eio, fibers created inside [with_binding k v (fun () -> …)] will inherit the
+      [k := v] assignment. This is consistent with the structured concurrency approach of
+      Eio.
+
+    The only data stored by this storage is a {!Hmap.t}, ie a heterogeneous map. Various
+    users (libraries, user code, etc.) can create their own {!key} to store what they are
+    interested in, without affecting other parts of the storage. *)
+
 type 'a key = 'a Hmap.key
 
 module type STORAGE = sig
   val name : string
+  (** Name of the storage implementation. *)
+
   val get_map : unit -> Hmap.t option
+  (** Get the hmap from the current ambient context, or [None] if there is no ambient
+      context. *)
+
   val with_map : Hmap.t -> (unit -> 'b) -> 'b
+  (** [with_hmap h cb] calls [cb()] in an ambient context in which [get_map()] will return
+      [h]. Once [cb()] returns, the storage is reset to its previous value. *)
+
   val create_key : unit -> 'a key
+  (** Create a new storage key, guaranteed to be distinct from any previously created key. *)
+
   val get : 'a key -> 'a option
   val with_binding : 'a key -> 'a -> (unit -> 'b) -> 'b
   val without_binding : 'a key -> (unit -> 'b) -> 'b
