f13cc610070782dbc2244c19826f56af11875de3
Simon Cruanes <simon.cruanes.2007@m4x.org>
2023-09-19 16:50:48 -0400

    docs

--- lib/ambient_context.mli
+++ lib/ambient_context.mli
@@ -46,8 +46,8 @@ val get : 'a key -> 'a option
     key in the ambient context. *)
 
 val with_binding : 'a key -> 'a -> (unit -> 'r) -> 'r
-(** [without_binding k v cb] calls [cb()] in a context in which [k] is bound to [v]. When
-    [cb()] returns, storage is restored to its previous state. *)
+(** [with_binding k v cb] calls [cb()] in a context in which [k] is bound to [v]. This
+    does not affect storage outside of [cb()]. *)
 
 val without_binding : 'a key -> (unit -> 'b) -> 'b
 (** [without_binding k cb] calls [cb()] in a context where [k] has no binding (possibly
